dc251eea261fb83ed90743862e77b4ada33050a0
Fernet97 <44033607+Fernet97@users.noreply.github.com>
2019-11-16 16:27:01 +0100

    package database + add lib

---
+++ StockManagement/src/ui/LoginDialog.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class LoginDialog extends javax.swing.JDialog {
+
+ 
+    
+    
+  
+    
+}
