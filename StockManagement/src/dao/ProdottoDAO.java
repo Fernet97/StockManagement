42ba9eedb11144125c0012401555c006d1bcd6a9
Vittorio Manisera <44257553+LittleJoke95@users.noreply.github.com>
2019-11-26 12:26:52 +0100

    update prodotto dao
    add getbysku & insert into

--- StockManagement/src/dao/ProdottoDAO.java
+++ StockManagement/src/dao/ProdottoDAO.java
@@ -63,4 +63,88 @@ public class ProdottoDAO {
                                                 return prodotti;
                                                 
     }
+      
+        public  synchronized Prodotto getBySku(int sku) throws SQLException{
+             
+            Connection connection = null;
+             PreparedStatement ps = null;
+             
+             Prodotto bean = new Prodotto();
+             //SELECT * FROM prodotti WHERE sku = 1;
+             String selectSQL = "SELECT * FROM " + this.TABLE_NAME + " WHERE sku = ?";
+             
+                 		try  {
+                                                        connection = DriverManagerConnectionPool.getConnection();
+                                                        ps = connection.prepareStatement(selectSQL);
+                                                        ps.setInt(1, sku);// FA RIFERIMENTO AL NOME ED AL NUMERO DELLA COLONNA NEL DB
+
+                                                        ResultSet rs = ps.executeQuery();
+                                                        
+                                                        while (rs.next()) {
+                                                                                                
+                                                                                                bean.setSku(rs.getString("sku"));
+                                                                                                bean.setDataReg(rs.getString("datareg"));
+                                                                                                bean.setNome(rs.getString("nome"));
+                                                                                                bean.setCategoria(rs.getString("categoria"));
+                                                                                                bean.setInStock(rs.getBoolean("instock"));
+                                                                                                bean.setDescrizione(rs.getString("descrizione"));
+                                                                                                bean.setGiorni_alla_consegna(rs.getInt("giorni_alla_consegna"));
+                                                                                                bean.setQty(rs.getInt("qty"));
+                                                                                                bean.setCosto(rs.getFloat("costo"));
+                                                                                                bean.setFoto(rs.getString("foto"));
+                                                                                                  
+                                                                                    }
+                                }
+                                                finally {
+			try {
+                                                                                if (ps != null)
+                                                                                    ps.close();
+                                                                            }
+                                                                          finally {
+                                                                                    DriverManagerConnectionPool.releaseConnection(connection);
+                                                                                    }
+                        
+                                                  }
+                                                return bean;
+                                }
+  
+        public synchronized void add(Prodotto b) throws SQLException{
+                         Connection connection = null;
+                        PreparedStatement ps = null;
+        
+                        String insertSQL= "INSERT INTO " + TABLE_NAME
+                                                                      + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";       
+                        
+                        try{
+                                    connection = DriverManagerConnectionPool.getConnection();
+                                    ps = connection.prepareStatement(insertSQL);     
+                                    
+                                    ps.setString(1, b.getSku());
+                                    ps.setString(2, b.getDataReg());
+                                    ps.setString(3, b.getNome());
+                                    ps.setString(4, b.getCategoria());
+                                    ps.setBoolean(5, b.isInStock());//tinyint
+                                    ps.setString(6, b.getDescrizione());
+                                    ps.setInt(7, b.getGiorni_alla_consegna());
+                                    ps.setInt(8, b.getQty());
+                                    ps.setFloat(9, b.getCosto());
+                                    ps.setString(10, b.getFoto());
+		
+                                    System.out.println(insertSQL);
+                                    
+                                    ps.executeUpdate();
+                                    
+                                    connection.commit();
+                        }
+                        finally{
+                                    try{
+                                            if (ps != null)
+                                                ps.close();
+                                    }
+                                    finally{
+                                                 DriverManagerConnectionPool.releaseConnection(connection);
+                                    }
+                        }
+        }
 }
+
