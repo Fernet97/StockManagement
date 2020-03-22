b78c4ae7bf36131afa9bff4340895e31bbcfa482
Vittorio Manisera <44257553+LittleJoke95@users.noreply.github.com>
2020-03-22 16:26:50 +0100

    aggiunti metodi per verificare spedizione
    is arrivato returna booleano se lórdine é arrivato
    
    ggconsegna ritorna int con i giorni mancanti alla consegna

---
+++ StockManagement/test/ordinedao/test3.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordinedao;
+
+import java.sql.SQLException;
+import dao.OrdineDAO;
+import beans.Ordine;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+
+/**
+ *
+ * @author LittleJoke
+ */
+public class test3 {
+
+    public static void main(String[] args) throws InterruptedException, SQLException, ParseException {
+Ordine bean = new Ordine();
+            OrdineDAO dao =new OrdineDAO();
+
+            
+//            System.out.println(dao.isArrivato("ord-1"));
+
+        System.out.println(dao.ggConsegna("ord-1"));
+            
+            
+    }
+}
+
